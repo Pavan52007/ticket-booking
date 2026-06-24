d4bb2662b8d12a91164b0775c595abbab02e6582
Pavan52007 <bommalapavansai26@gmail.com>
2026-06-24 15:05:42 +0530

    Added deployment configuration

--- EventSphere/settings.py
+++ EventSphere/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xre79go5*=fmtt-*7w%9m)uo_y(+=uqpyg(6^k@cpmb!(yrl%n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
